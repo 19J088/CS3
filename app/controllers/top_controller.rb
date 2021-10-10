342c5f8dd1bf1a2cb8a7a2135fc5e670369864a4
Ubuntu <ubuntu@ip-172-31-67-238.ec2.internal>
2021-10-10 12:14:53 +0000

    CS3

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,27 @@
+class TopController < ApplicationController
+    def main
+        if session[:login_uid]
+            render 'main'
+        else
+            render 'login'
+        end
+    end
+    
+    def login
+        if User.find_by(uid: params[:uid])
+            signup_password = BCrypt::Password.create(params[:pass])
+            if BCrypt::Password.new(signup_password) == params[:pass]
+                p "ログイン成功"
+               session[:login_uid] = params[:uid]
+               redirect_to root_path
+       else
+           render 'login_failed'
+       end
+   end
+   def logout
+       session.delete(:login_uid)
+       redirect_to root_path
+   end
+   end
+   end
+   
